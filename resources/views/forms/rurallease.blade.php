c64b346af30d0466d954ed0d15a5f4977cac9134
TawonaRwatidaR204712J <tnrwatida@gmail.com>
2023-05-12 09:43:08 +0200

    remake rurallease

--- resources/views/forms/rurallease.blade.php
+++ resources/views/forms/rurallease.blade.php
@@ -11,7 +11,7 @@
     table {
         /*border-top: 1px solid;
         border-bottom: 1px solid;*/
-        width: 70%;
+        width: 100%;
         font-family: 'Arial Narrow', Arial, sans-serif;
         font-size: 11.5pt;
     }
@@ -64,7 +64,7 @@
     p {
         line-height: 0.6cm;
         font-family: 'Arial Narrow', Arial, sans-serif;
-        font-size: 12.5pt;
+        font-size: 11.5pt;
     }
 </style>
 <div id="border">
@@ -97,55 +97,64 @@
         <center>
             <p id="lease-header">
                 <strong>
-                    COMMUNAL LAND LEASE
+                    APPLICATION FOR LEASE FOR TRADING AND LIKE PURPOSES IN COMMUNAL LAND
                 </strong>
             </p>
+            <p>
+                I hereby make application for (a) lease site (b) a cession of a leased site (c) an additional business one a leased <br>
+                site (d) a deletion of a business from a leased site. (e) an alteration of business on a leased site (f) a change of <br>
+                site (g) a sub-lease.
+            </p>
+            <p>(DELETE THE INAPPLIABLE)</p>
         </center>
-        <p>LEASE NUMBER: {{$rurallease->lease_no}}</p>
-        <p>BUSINESS: {{$rurallease->stand_purpose}}</p>
-        <p>COMMUNAL LAND: {{$rurallease->area}}</p>
-        <p>ADDRESS: {{$rurallease->person->address}}</p>
-        <p>CONTACT DETAILS: {{$rurallease->person->mobile}}_________NEXT OF KIN CONTACT: {{$rurallease->person->nok->telephone}}</p>
-        <p>DISTRICT: TSHOLOTSHO RURAL DISTRICT <br> ZIMBABWE</p>
-        <p>COMMUNAL LAND LEASE SITE <br>TRADING OR OTHER PURPOSES</p>
         <table>
             <tr>
-                <td>Witnesses:</td>
+                <td>1. District: TSHOLOTSHO RURAL DISTRICT COUNCIL</td>
+                <td>Communal Land: {{$rurallease->area}}</td>
             </tr>
             <tr>
-                <td>1. ___________________________</td>
-                <td>ID Number: ________________________</td>
-                <td>Signature: _________________</td>
+                <td>Surname or company name: {{$rurallease->person->surname}}</td>
             </tr>
             <tr>
-                <td>2. ___________________________</td>
-                <td>ID Number: ________________________</td>
-                <td>Signature: _________________</td>
+                <td>Other names: {{$rurallease->person->firstname}}</td>
             </tr>
             <tr>
-                <td>Lessor (Chief Executive Officer)</td>
+                <td>Identity Card Number: {{$rurallease->person->nationalid}}</td>
+                <td>District: TSHOLOTSHO RURAL DISTRICT/____________________</td>
+            </tr>
+            <tr>
+                <td>Postal Address: {{$rurallease->person->postaladdress}}</td>
             </tr>
         </table>
-        <p><strong>For Tsholotsho Rural District Council</strong> <br>
-            I certify that the terms of this lease have fully been explained to me and acknowledge that I understand the provisions thereof. <br>
-            Dated at this _________________ day of _________________/20______</p>
-        <table>
-            <tr>
-                <td>Witness:</td>
-            </tr>
-            <tr>
-                <td>1. ___________________________</td>
-                <td>ID Number: ________________________</td>
-                <td>Signature: _________________</td>
-            </tr>
-            <tr>
-                <td>2. ___________________________</td>
-                <td>ID Number: ________________________</td>
-                <td>Signature: _________________</td>
-            </tr>
-            <tr>
-                <td>Lessor (Chief Executive Officer)</td>
-            </tr>
-        </table>
+        <p>(If a company, a seperate advice must be attached giving (a) the full names of each and every director (b) their birthplace and date of birth (c) their dates of entry into Zimbabwe, and places of entry (d) whether or not they hold Zimbabwean citizenship.)</p>
+        <p>3. Managerial details: Surname ______________________ <br>
+            Other names:_______________________ <br>
+            Identity No._______________________ District_____________________</p>
+        <p>4. What capital is available to you?___________________________ <br>
+            Are you being financed in whole or in part by some other person? YES/NO <br>
+            If yes state amount $________________ and Name of Financier_____________________</p>
+        <p>5. Locality of site (name of business centre, kraal, school, dip etc): {{$rurallease->area}} <br>
+            if NOT a business centre state distance in kilometres to neareast township or business centre_____________________ and give reasons for sitting outside a business centre. <br>
+            ______________________________________________________________________________________________________________________________________________________________________________ <br>
+            _______________________________________________________________________________________________________________________________________________________________________________</p>
+        <p>6. Nature of proposed business: {{$rurallease->stand_purpose}}</p>
+        <p>7. Have you any other leases in Communal Land? YES/NO if YES state_________________ <br>
+            No._______________________ and advice Receipt Number and amount covering payment of current rental________________________________________________________</p>
+        <p>I certify that the information given above is true and correct and that I have not withheld and information that may way affect my application. <br>
+            DATE: ____________/_____________/_______________ SIGNATURE OF APPLICANT:______________________</p>
+        <p style="text-decoration: underline;"><strong>SCHEDULE</strong> <br>
+            The Chief Executive Officer <br>
+            ______________________________</p>
+        <p>10. Recommended area site of: _____________________ Square meters.</p>
+        <p>11. The sites provisionary allocated has been approved by the Local Planning Authority. <br>
+            recommend that an offer for lease be made.</p>
+        <p>DATE: ___________/______________/____________ STATION: _________________ SIGNATURE: _______________</p>
+        <p>TO BE COMPLETED IN ALL CASES OTHER THAN FOR NEW LEASES CERTIFIED THAT RENT FROM CURRENCT YEAR 20__________ IN THE SUM OF________________ <br>
+            HAS BEEN PAID UNDER RECEIPT NO_______________ <br>
+            DATED_____________________</p>
+        <p>DATE: ________/__________/_____________ FOR RURAL DISTRICT COUNCIL:______________________</p>
+        <p>In supprt of the above I attach. <br>
+            1.Sketch maps in the duplicate of layout of the site showing all adjacent leased sites both vacant and leased, with numbers supplied for the letter. <br>
+            2. Sketch maps showing location of site within the communal land.</p>
     </div>
 </div>
